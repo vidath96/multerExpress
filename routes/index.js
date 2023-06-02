20595b2e432e4a250b857574f83fc5d68c486918
vidath96 <vidathhasantha96@gmail.com>
2023-06-02 16:59:47 +0530

    File Upload

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+const multer = require('multer')
+const upload = multer({dest:"public/images"})
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.post('/fileformsub',upload.single('avatar'),(req, res, next) => {
+
+  res.json(req.body)
+})
 
 module.exports = router;
